bc799a4d6b42cb2b61d8e8898da7b0b7acde7d92
Corey Acri <acri.corey@gmail.com>
2017-04-08 15:37:46 -0400

    added movie blade

---
+++ resources/views/meetup.blade.php
@@ -0,0 +1,13 @@
+<?php
+namespace App; 
+
+use Illuminate\Database\Eloquent\Model; 
+
+// laravel knws that shince this model is named MOvie, 
+// its database table is "movies"
+
+class Meetup extends Model {
+
+
+
+}
